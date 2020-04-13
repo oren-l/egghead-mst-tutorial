9c23187512ff63d4ef8cc25c9d8e65dd537d25cc
Oren Levi <orenl@cympire.com>
2020-04-13 18:28:58 +0300

    Remove cra stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from 'assets/santa-claus.png'
 
 function App () {
   return (
@@ -16,7 +15,7 @@ function App () {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          <h1>Learn React</h1>
         </a>
       </header>
     </div>
