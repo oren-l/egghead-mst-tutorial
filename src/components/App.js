a23f31526c11f0cbcb1cd858be3e2c0771c37bf4
Oren Levi <oren.stu@gmail.com>
2020-05-02 18:47:13 +0300

    Finish lesson-15

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ function App ({
         <img src={logo} className='App-logo' alt='logo' />
         <h1 className='App-title'>WishList</h1>
       </header>
+      <button onClick={group.reload}>Reload</button>
       <select
         onChange={handleSelectUser}
         defaultValue={0}
